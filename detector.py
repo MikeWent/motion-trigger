afcac32156b1f8308faab1bf9f275f3bc4ea368f
Mike_Went <git@meew.me>
2018-11-26 22:36:31 +0300

    Add the main feature — GPIO switch

--- detector.py
+++ detector.py
@@ -1,17 +1,57 @@
 #!/usr/bin/env python3
 
-import cv2
-import numpy as np
-import time
 import argparse
+import time
+from os.path import exists as path_exists
+import configparser
+
+import numpy as np
+
+import cv2
 
 args = argparse.ArgumentParser()
-args.add_argument("-t", "--threshold", help="motion level to trigger an action", type=int, default=10)
+args.add_argument("-t", "--threshold", help="motion level to trigger an action (overrides a value in the config file)", type=int)
+args.add_argument("-c", "--cooldown", help="cooldown in seconds to wait before switching the pin back to the default state (overrides a value in the config file)", type=int)
 args.add_argument("--debug", help="show the video input and motion diff", action="store_true")
 options = args.parse_args()
 
-print(f"Using OpenCV {cv2.__version__}")
+config = configparser.ConfigParser()
+config.read("config.ini")
 
+THRESHOLD = options.threshold if options.threshold else int(config["defaults"].get("threshold", 10))
+COOLDOWN = options.cooldown if options.cooldown else int(config["defaults"].get("cooldown", 30))
+
+def timestamp():
+    """Get current UNIX time"""
+    return int(time.time())
+
+
+def gpio_set(pin, value):
+    """Set GPIO pin to value
+    
+    https://github.com/MikeWent/gpio-remote-control/blob/master/server.py
+    """
+    if not path_exists("/sys/class/gpio/gpio{}".format(pin)):
+        # Export pin
+        with open("/sys/class/gpio/export", "w") as f:
+            f.write(str(pin))
+        time.sleep(0.05)
+        # Set pin direction to "out"
+        with open("/sys/class/gpio/gpio{}/direction".format(pin), "w") as f:
+            f.write("out")
+        time.sleep(0.05)
+    # Set value
+    with open("/sys/class/gpio/gpio{}/value".format(pin), "w") as f:
+        f.write(str(value))
+
+    if options.debug:
+        print(f"Pin {pin} is switched to {value}")
+
+
+print(f"Using OpenCV {cv2.__version__}")
+print(f"Cooldown is {COOLDOWN} sec.")
+print(f"Threshold is {THRESHOLD}")
+print("===")
 # This code is basically an improved version of https://software.intel.com/en-us/node/754940
 
 if options.debug:
@@ -34,6 +74,8 @@ cap = cv2.VideoCapture(0)
 _, frame1 = cap.read()
 _, frame2 = cap.read()
 print("Motion detection is started.")
+last_time_motion_detected = 0
+pin_state = "default"
 
 while(True):
     _, frame3 = cap.read()
@@ -63,8 +105,21 @@ while(True):
         if cv2.waitKey(1) & 0xFF == 27:
             break
 
-    if st_dev > options.threshold:
+    if st_dev > THRESHOLD:
         print(f"Motion detected! Level: {round(st_dev, 1)}")
+        last_time_motion_detected = timestamp()
+        gpio_set(config["action"]["pin"], config["action"]["on_motion"])
+        pin_state = "non-default"
+
+    if last_time_motion_detected + COOLDOWN < timestamp() and pin_state == "non-default":
+        print("Cooldown reached.")
+        default_value = 1 if config["action"]["on_motion"] == "0" else 0
+        gpio_set(config["action"]["pin"], default_value)
+        pin_state = "default"
+        # skip next 10 frames to prevent loop because light switch triggers motion detector
+        for _ in range(0, 10):
+            _, _frame_ = cap.read()
+
 
 cap.release()
 cv2.destroyAllWindows()
