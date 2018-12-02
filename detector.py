21d2d530d471a9cac1f7110837f36a787860c573
Mike_Went <git@meew.me>
2018-12-02 21:58:58 +0300

    Code cleanup, add brightness to config

--- detector.py
+++ detector.py
@@ -22,10 +22,11 @@ THRESHOLD = options.threshold if options.threshold else int(config["defaults"].g
 COOLDOWN = options.cooldown if options.cooldown else int(config["defaults"].get("cooldown", 30))
 HEIGHT = int(config["webcam"]["height"])
 WIDTH = int(config["webcam"]["width"])
+BRIGHTNESS = int(config["webcam"]["brightness"])
 
 
 def timestamp():
-    """Get current UNIX time"""
+    """Current UNIX time"""
     return int(time.time())
 
 
@@ -34,6 +35,10 @@ def gpio_set(pin, value):
     
     https://github.com/MikeWent/gpio-remote-control/blob/master/server.py
     """
+    if options.debug:
+        print(f"Pin {pin} is switched to {value}")
+        return
+    
     if not path_exists("/sys/class/gpio/gpio{}".format(pin)):
         # Export pin
         with open("/sys/class/gpio/export", "w") as f:
@@ -47,72 +52,72 @@ def gpio_set(pin, value):
     with open("/sys/class/gpio/gpio{}/value".format(pin), "w") as f:
         f.write(str(value))
 
-    if options.debug:
-        print(f"Pin {pin} is switched to {value}")
+
+def diff_img(t0, t1, t2):
+    """Difference between three images"""
+    d1 = cv2.absdiff(t2, t1)
+    d2 = cv2.absdiff(t1, t0)
+    return cv2.bitwise_and(d1, d2)
+
+
+def get_frame():
+    """Retrive a graysacle frame from capturing device (webcam)"""
+    global cap
+    _, frame = cap.read()
+    return cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
 
 print(f"# OpenCV {cv2.__version__}")
 print(f"# Threshold  = {THRESHOLD}")
 print(f"# Cooldown   = {COOLDOWN} sec")
-print(f"# Resolution = {HEIGHT}x{WIDTH} px\n")
-
-# This code is basically an improved version of https://software.intel.com/en-us/node/754940
+print(f"# Resolution = {WIDTH}x{HEIGHT} px\n")
 
 if options.debug:
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.namedWindow('frame')
-    cv2.namedWindow('dist')
-
-def distMap(frame1, frame2):
-    """Pythagorean distance between two frames"""
-    frame1_32 = np.float32(frame1)
-    frame2_32 = np.float32(frame2)
-    diff32 = frame1_32 - frame2_32
-    norm32 = np.sqrt(diff32[:,:,0]**2 + diff32[:,:,1]**2 + diff32[:,:,2]**2)/np.sqrt(255**2 + 255**2 + 255**2)
-    dist = np.uint8(norm32*255)
-    return dist
+    cv2.namedWindow("motion", cv2.WINDOW_AUTOSIZE)
+    cv2.namedWindow("raw", cv2.WINDOW_AUTOSIZE)
 
 # capture video stream from camera source. 0 refers to first camera, 1 refers to 2nd and so on.
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)
+cap.set(cv2.CAP_PROP_BRIGHTNESS, BRIGHTNESS)
 
-_, frame1 = cap.read()
-_, frame2 = cap.read()
+frame1 = get_frame()
+frame2 = get_frame()
 print("Motion detection is started.")
 last_time_motion_detected = 0
 pin_state = "default"
 frames_to_skip = 0
 
 while(True):
-    _, frame3 = cap.read()
-    rows, cols, _ = np.shape(frame3)
+    frame3 = get_frame()
     if options.debug:
-        cv2.imshow('dist', frame3)
-    dist = distMap(frame1, frame3)
+        cv2.imshow("raw", frame3)
 
-    frame1 = frame2
-    frame2 = frame3
-
+    # Skip frames if needed
     if frames_to_skip > 0:
         frames_to_skip -= 1
         continue
 
-    # apply Gaussian smoothing
-    mod = cv2.GaussianBlur(dist, (9,9), 0)
+    # Calculate diff between frames
+    diff = diff_img(frame1, frame2, frame3)
 
-    # apply thresholding
-    _, thresh = cv2.threshold(mod, 100, 255, 0)
+    # Shift frames
+    frame1 = frame2
+    frame2 = frame3
 
-    # calculate st dev test
-    _, st_dev = cv2.meanStdDev(mod)
+    # Apply Gaussian smoothing
+    blurred = cv2.GaussianBlur(diff, (13,13), 0)
+
+    # Apply thresholding
+    _, thresh = cv2.threshold(blurred, 10, 255, cv2.THRESH_BINARY)
+
+    # Calculate st dev
+    _, st_dev = cv2.meanStdDev(thresh)
     st_dev = st_dev[0][0]
 
     if options.debug:
-        cv2.imshow('dist', mod)
-        cv2.putText(frame2, "Standard Deviation = {}".format(round(st_dev, 0)), (70, 70), font, 1, (255, 0, 255), 1, cv2.LINE_AA)
-        cv2.imshow('frame', frame2)
-
+        cv2.imshow("motion", thresh)
         if cv2.waitKey(1) & 0xFF == 27:
             break
 
@@ -127,9 +132,8 @@ while(True):
         default_value = 1 if config["action"]["on_motion"] == "0" else 0
         gpio_set(config["action"]["pin"], default_value)
         pin_state = "default"
-        # skip next 10 frames to prevent loop because light switch triggers motion detector
+        # Skip next 10 frames to prevent loop because light switch triggers motion detector
         frames_to_skip = 10
-
 
 cap.release()
 cv2.destroyAllWindows()
